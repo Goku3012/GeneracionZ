b231430aac22bedb7322f58b3c966b41e49bb7c4
QuinteroNetzaret <netzaret.quintero.orellana.utelvt.edu.ec>
2022-04-25 14:25:51 -0500

    Trabaja de suma de numeros

---
+++ QuinteroAdonis/ACTIVIDAD-B2/QuinteroAdonis-SumaN.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int c=0,a;
+	float s=0,z;
+	cout<<"Ingrese la cantidad de numeros a sumar: ";
+	cin>>a;
+	do{
+	cout<<"Ingrese un numero: ";
+	cin>>z;
+	c=c+1;
+	s=s+z;
+	}while(c<a);
+	cout<<"La suma total es: "<<5<<endl;
+	return 0;
+}
+
