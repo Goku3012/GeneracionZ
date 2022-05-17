f2602790a46a4402f2aade7fc3f97d7e5b0eb37b
DanielaMontesdeoca <mishellecmontesdeoca@gmail.com>
2022-05-17 00:10:51 -0500

    Actualizacion C2

--- DanielaMontesdeoca/Actividad-B2-C2/MontesdeocaDaniela-Compara.cpp
+++ DanielaMontesdeoca/Actividad-B2-C2/MontesdeocaDaniela-Compara.cpp
@@ -1,19 +1,35 @@
+//==============================================
+//===>Nombre del programa: Comparacion de 2 nuneros
+//===>Archivo:Compara.cpp
+//===>Autor:Montesdeoca Castillo Daniela Mishelle
+//===>Fecha de elaboracion:02/05/2022
+//===>Fecha de Actualizacion:16/05/2022
+//===============================================
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	float x,y;
+	float MD_x,MD_y;
 	cout<<"Ingrese el valor de X: ";
-	cin>>x;
+	cin>>MD_x;
 	cout<<"Ingrese el valor de Y: ";
-	cin>>y;
-	if(x==y){
-	cout<<"El valor de X: "<<x<<"es igual a Y: "<<y<<endl;
+	cin>>MD_y;
+	if(MD_x==MD_y){
+	cout<<"El valor de X: "<<MD_x<<"es igual a Y: "<<MD_y<<endl;
 	}else{
-	if(x<y){
-	cout<<"El valor de X: "<<x<<"es menor a Y:"<<y<<endl;
+	if(MD_x<MD_y){
+
+cout<<"//=============================================="<<endl;
+cout<<"//===>Nombre del programa: Comparacion de 2 nuneros"<<endl;
+cout<<"//===>Archivo:Compara.cpp"<<endl;
+cout<<"//===>Autor:Montesdeoca Castillo Daniela Mishelle"<<endl;
+cout<<"//===>Fecha de elaboracion:02/05/2022"<<endl;
+cout<<"//===>Fecha de Actualizacion:16/05/2022"<<endl;
+cout<<"//==============================================="<<endl;
+	cout<<"El valor de X: "<<MD_x<<"es menor a Y:"<<MD_y<<endl;
 	}else{
-	cout<<"El valor de Y: "<<y<<"es menor a X:"<<x<<endl;
+	cout<<"El valor de Y: "<<MD_y<<"es menor a X:"<<MD_x<<endl;
 	}
 	}
 	return 0;
