fdc7e2fb80355ed3e1473b71fb6cdf7e30f3b395
QuinteroNetzaret <netzaret.quintero.orellana@utelvt.edu.ec>
2022-05-18 13:48:37 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- QuinteroAdonis/ACTIVIDAD-B2-C2/QuinteroAdonis-Compara.cpp
+++ QuinteroAdonis/ACTIVIDAD-B2-C2/QuinteroAdonis-Compara.cpp
@@ -1,19 +1,34 @@
+//================================================
+//==> Nombre del programa:Comparacion de 2 numeros
+//==> Archivo : Compara.cpp
+//==>Autor: Quintero Orellana Nezareth Adonis
+//==>Fecha de elaboración: 25/04/2022
+//==>Fecha ultima actualización: 18/05/2022
+//=================================================
 #include<iostream>
 using namespace std;
 int main()
 {
-	float x,y;
+	
+cout<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa:Comparacion de 2 numeros"<<endl;
+cout<<"//==> Archivo : Compara.cpp"<<endl;
+cout<<"//==>Autor: Quintero Orellana Nezareth Adonis"<<endl;
+cout<<"//==>Fecha de elaboración: 25/04/2022"<<endl;
+cout<<"//==>Fecha ultima actualización: 18/05/2022"<<endl;
+cout<<"//================================================="<<endl<<endl;
+	float ON_x,ON_y;
 	cout<<"Ingrese el valor de X: ";
-	cin>>x;
+	cin>>ON_x;
 	cout<<"Ingrese el valor de Y: ";
-	cin>>y;
-	if(x==y){
-	cout<<"El valor de X: "<<x<<"es igual a Y: "<<y<<endl;
+	cin>>ON_y;
+	if(ON_x==ON_y){
+	cout<<"El valor de X: "<<ON_x<<"es igual a Y: "<<ON_y<<endl;
 	}else{
-	if(x<y){
-	cout<<"El valor de X: "<<x<<"es menor a Y: "<<y<<endl;
+	if(ON_x<ON_y){
+	cout<<"El valor de X: "<<ON_x<<"es menor a Y: "<<ON_y<<endl;
 	}else{
-	cout<<"El valor de Y: "<<y<<"es menor a X: "<<x<<endl;
+	cout<<"El valor de Y: "<<ON_y<<"es menor a X: "<<ON_x<<endl;
 	}
 	}
 	return 0;
