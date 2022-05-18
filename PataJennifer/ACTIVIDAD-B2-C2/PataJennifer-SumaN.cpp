d4a3f960a66f7d683bdce924e034c1b9979d494e
Goku3012 <jennifer.pata.mina@utelvt.edu.ec>
2022-05-18 08:38:58 -0500

    Actualización ACTIVIDAD-B2-C2

--- PataJennifer/ACTIVIDAD-B2-C2/PataJennifer-SumaN.cpp
+++ PataJennifer/ACTIVIDAD-B2-C2/PataJennifer-SumaN.cpp
@@ -1,17 +1,44 @@
+//===============================================================================================================
+//============> Nombre del Programa: Suma de Varios Números
+//============> Archivo: SumaN.cpp
+//============> Autor: Pata Mina Jennifer Maibe
+//============> Fecha de Creación: 23/04/2022
+//============> Fecha de Actualización: 18/05/2022
+//===============================================================================================================
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int C=0,a;
-	float S=0,z;
-	cout<<"Ingrese la cantidad de numeros a sumar: ";
-	cin>>a;
+
+cout<<"//==============================================================================================================="<<endl;
+cout<<"//============> Nombre del Programa: Suma de Varios Números"<<endl;
+cout<<"//============> Archivo: SumaN.cpp"<<endl;
+cout<<"//============> Autor: Pata Mina Jennifer Maibe"<<endl;
+cout<<"//============> Fecha de Creación: 23/04/2022"<<endl;
+cout<<"//============> Fecha de Actualización: 18/05/2022"<<endl;
+cout<<"//==============================================================================================================="<<endl<<endl;
+
+	int MJ_c=0,MJ_n;
+
+	float MJ_a=0,MJ_x;
+
+	cout<<"Ingrese la Cantidad de Números a Sumar: ";
+	cin>>MJ_n;
+
 	do{
-	cout<<"Ingrese un numero: ";
-	cin>>z;
-	C=C+1;
-	S=S+z;
-	}while(C<a);
-	cout<<"La suma total es: "<<S<<endl;
+
+	cout<<"Ingrese un Número: ";
+	cin>>MJ_x;
+
+	MJ_c=MJ_c+1;
+
+	MJ_a=MJ_a+MJ_x;
+
+	}while(MJ_c<MJ_n);
+
+	cout<<"La Suma Total es: "<<MJ_a<<endl;
+
 	return 0;
 }
