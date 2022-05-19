6f29dfd87d9c5d8ef3a720ce19568219a14c2a6a
Goku3012 <jennifer.pata.mina@utelvt.edu.ec>
2022-05-19 01:51:36 -0500

    Actualización ACTIVIDAD-B2-C2

--- PataJennifer/ACTIVIDAD-B2-C2/PataJennifer-CuentaMoneda.cpp
+++ PataJennifer/ACTIVIDAD-B2-C2/PataJennifer-CuentaMoneda.cpp
@@ -1,36 +1,68 @@
+//===============================================================================================================
+//=======> Nombre del Programa: Cuenta Monedas "DonNaza"
+//=======> Archivo: CuentaMoneda.cpp
+//=======> Autor: Pata Mina Jennifer Maibe
+//=======> Fecha de Creación:23/04/2022
+//=======> Fecha de Actualización:19/05/2022
+//===============================================================================================================
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int n,c=0,c1=0,c2=0,c3=0;
-	float x,a=0,a1=0,a2=0,a3=0;
+
+cout<<"//==============================================================================================================="<<endl;
+cout<<"//=======> Nombre del Programa: Cuenta Monedas DonNaza"<<endl;
+cout<<"//=======> Archivo: CuentaMoneda.cpp"<<endl;
+cout<<"//=======> Autor: Pata Mina Jennifer Maibe"<<endl;
+cout<<"//=======> Fecha de Creación:23/04/2022"<<endl;
+cout<<"//=======> Fecha de Actualización:19/05/2022"<<endl;
+cout<<"//==============================================================================================================="<<endl;
+
+	int MJ_n,MJ_c=0,MJ_c1=0,MJ_c2=0,MJ_c3=0;
+
+	float MJ_x,MJ_a=0,MJ_a1=0,MJ_a2=0,MJ_a3=0;
+	
 	cout<<"Bienvenidos al Cuenta Monedas Don Naza "<<endl<<endl;
+	
 	cout<<"Ingrese la Cantidad de monedas a contar: "<<endl<<endl;
-	cin>>n;
+	cin>>MJ_n;
+	
 	do{
-	cout<<"Ingrese su valor en monedas (0.10,0.25,0.50):$ "<<endl;
-	cin>>x;
-	c=c+1;
-	a=a+x;
-	if(x==(float)0.10){
-		c1=c1+1;
-		a1=a1+x;
-	}else if(x==(float)0.25){
-		c2=c2+1;
-		a2=a2+x;
+	
+		cout<<"Ingrese su valor en monedas (0.10,0.25,0.50):$ "<<endl;
+	        cin>>MJ_x;
+
+        	MJ_c=MJ_c+1;
+         	MJ_a=MJ_a+MJ_x;
+	
+	if(MJ_x==(float)0.10){
+	
+		MJ_c1=MJ_c1+1;
+		MJ_a1=MJ_a1+MJ_x;
+
+	}else if(MJ_x==(float)0.25){
+		
+		MJ_c2=MJ_c2+1;
+		MJ_a2=MJ_a2+MJ_x;
+
 	}else{
-		c3=c3+1;
-		a3=a3+x;}
-	}while(c<n);
+		
+		MJ_c3=MJ_c3+1;
+		MJ_a3=MJ_a3+MJ_x;}
+
+	}while(MJ_c<MJ_n)
+	;
 	cout<<"El Resultado Fue: "<<endl<<endl;
-	cout<<"Total de Monedas Contadas: "<<c<<endl<<endl;
-	cout<<"Valor Total en Efectivo:$ "<<a<<endl<<endl;
-	cout<<"Cantidad de monedas de 0.10ctvs: "<<c1<<endl<<endl;
-	cout<<"Valor en Efectivo de monedas de 0.10ctvs:$ "<<a1<<endl;
-	cout<<"Cantidad de monedas de 0.25ctvs: "<<c2<<endl<<endl;
-	cout<<"Valor en Efectivo de monedas de 0.25ctvs:$ "<<a2<<endl;
-	cout<<"Cantidad de monedas de 0.50ctvs: "<<c3<<endl<<endl<<endl;
-	cout<<"Valor en Efectivo de monedas de 0.50ctvs:$ "<<a3<<endl;
+	
+	cout<<"Total de Monedas Contadas: "<<MJ_c<<endl<<endl;
+	cout<<"Valor Total en Efectivo:$ "<<MJ_a<<endl<<endl;
+	cout<<"Cantidad de monedas de 0.10ctvs: "<<MJ_c1<<endl;
+	cout<<"Valor en Efectivo de monedas de 0.10ctvs:$ "<<MJ_a1<<endl<<endl;
+	cout<<"Cantidad de monedas de 0.25ctvs: "<<MJ_c2<<endl;
+	cout<<"Valor en Efectivo de monedas de 0.25ctvs:$ "<<MJ_a2<<endl<<endl;
+	cout<<"Cantidad de monedas de 0.50ctvs: "<<MJ_c3<<endl<<endl;
+	cout<<"Valor en Efectivo de monedas de 0.50ctvs:$ "<<MJ_a3<<endl;
 
 	return 0;
 }
