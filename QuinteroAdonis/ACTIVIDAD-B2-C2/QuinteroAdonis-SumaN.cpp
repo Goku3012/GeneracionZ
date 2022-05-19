1a103d6edd1550f3b908b3709816361efd7be671
QuinteroNetzaret <netzaret.quintero.orellana@utelvt.edu.ec>
2022-05-19 08:08:33 -0500

    Actualizacion ACTIVIDAD-B2-C2

--- QuinteroAdonis/ACTIVIDAD-B2-C2/QuinteroAdonis-SumaN.cpp
+++ QuinteroAdonis/ACTIVIDAD-B2-C2/QuinteroAdonis-SumaN.cpp
@@ -1,17 +1,34 @@
+//================================================
+//==> Nombre del programa: Suma de dos número
+//==> Archivo : SumaN.cpp
+//==>Autor: Quintero Orellana Nezareth Adonis
+//==>Fecha de elaboración: 25/04/2022
+//==>Fecha ultima actualización: 19/05/2020
+//=================================================
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int C=0,a;
-	float S=0,Y;
+	int ON_C=0,ON_a;
+	float ON_S=0,ON_Y;
 	cout<<"Ingrese la cantidad de numeros a sumar: ";
-	cin>>a;
+	cin>>ON_a;
 do{
 	       cout<<"Ingrese un numero: ";
-	       cin>>Y;
-	       C=C+1;
-	       S=S+Y;
-	}while(C<a);
-cout<<"La suma total es: "<<S<<endl;
+	       cin>>ON_Y;
+	       ON_C=ON_C+1;
+	       ON_S=ON_S+ON_Y;
+	}while(ON_C<ON_a);
+
+cout<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa: Suma de dos número"<<endl;
+cout<<"//==> Archivo : SumaN.cpp"<<endl;
+cout<<"//==>Autor: Quintero Orellana Nezareth Adonis"<<endl;
+cout<<"//==>Fecha de elaboración: 25/04/2022"<<endl;
+cout<<"//==>Fecha ultima actualización: 19/05/2020"<<endl;
+cout<<"//================================================="<<endl;
+
+cout<<"La suma total es: "<<ON_S<<endl;
 return 0;
 }
