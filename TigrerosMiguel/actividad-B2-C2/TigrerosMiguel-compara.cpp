ef4e7a877058943312ef4fd90bc2e2f926503f67
MiguelTigreros17 <miguel.tigreros.estacio@utelvt.edu.ec>
2022-05-19 22:42:17 -0500

    ActualizacionC2

--- TigrerosMiguel/actividad-B2-C2/TigrerosMiguel-compara.cpp
+++ TigrerosMiguel/actividad-B2-C2/TigrerosMiguel-compara.cpp
@@ -1,19 +1,35 @@
-# include <iostream>
+//===============================================
+//===>Nombre del programa: Comparacion de 2 numeros
+//===>Archivo:compara.cpp
+//===>Autor:Tigreros Estacio Miguel
+//===>Fecha de elaboracion:03/05/2022
+//===>Fecha de actualizacion:19/05/2022
+//===============================================
+
+#include <iostream>
 using namespace std;
 int main ()
 {
-	float a,b;
+	float TM_a,TM_b;
 	cout<<"Ingrese el valor de A: ";
-	cin>>a;
+	cin>>TM_a;
 	cout<<"Ingrese el valor de B: ";
-	cin>>b;
-	if (a==b){
-		cout<<"El valor de A: "<<a<<"es igual a B: "<<b<<endl;
+	cin>>TM_b;
+	if (TM_a==TM_b){
+		cout<<"El valor de A: "<<TM_a<<"es igual a B: "<<TM_b<<endl;
 	} else {
-		if (a<b){
-			cout<<"El valor de A: "<<a<<"es menor a B: "<<b<<endl;
+		if (TM_a<TM_b){
+
+cout<<"//==============================================="<<endl;
+cout<<"//===>Nombre del programa: Comparacion de 2 numeros"<<endl;
+cout<<"//===>Archivo:compara.cpp"<<endl;
+cout<<"//===>Autor:Tigreros Estacio Miguel"<<endl;
+cout<<"//===>Fecha de elaboracion:03/05/2022"<<endl;
+cout<<"//===>Fecha de actualizacion:19/05/2022"<<endl;
+cout<<"//==============================================="<<endl;
+			cout<<"El valor de A: "<<TM_a<<"es menor a B: "<<TM_b<<endl;
 		} else {
-			cout<<"El valor de B: "<<b<<"es menor a A: "<<a<<endl;
+			cout<<"El valor de B: "<<TM_b<<"es menor a A: "<<TM_a<<endl;
 		}
 	}
 	return 0;
