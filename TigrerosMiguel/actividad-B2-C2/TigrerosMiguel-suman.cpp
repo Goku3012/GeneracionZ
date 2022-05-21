5a5bd99bce72534ef0b146ef9d6548fb500b0ea8
MiguelTigreros17 <miguel.tigreros.estacio@utelvt.edu.ec>
2022-05-21 15:03:28 -0500

    Actualizacion-C2

--- TigrerosMiguel/actividad-B2-C2/TigrerosMiguel-suman.cpp
+++ TigrerosMiguel/actividad-B2-C2/TigrerosMiguel-suman.cpp
@@ -1,3 +1,11 @@
+//=========================================
+//===>Nombre del programa:Calcular la suma de dos numeros
+//===>Autor:Tigreros Escobar Miguel
+//===>Archivo:suman.cpp
+//===>Fecha de elaboracion:03/05/2022
+//===>Fecha de actualizacion:21/05/2022
+//=========================================
+
 #include<iostream>
 using namespace std;
 int main()
@@ -12,6 +20,15 @@ do {
 		C=C+1;
 		S=S+Y;
 	} while (C<a);
+
+cout<<"//========================================="<<endl;
+cout<<"//===>Nombre del programa:Calcular la suma de dos numeros"<<endl;
+cout<<"//===>Autor:Tigreros Escobar Miguel"<<endl;
+cout<<"//===>Archivo:suman.cpp"<<endl;
+cout<<"//===>Fecha de elaboracion:03/05/2022"<<endl;
+cout<<"//===>Fecha de actualizacion:21/05/2022"<<endl;
+cout<<"//========================================="<<endl;
+
 cout<<"La suma total es: "<<S<<endl;
 return 0;
 }
