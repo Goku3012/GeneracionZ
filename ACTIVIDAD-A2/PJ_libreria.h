42afc790c662efd82047b7cc34a83498cd2533fd
Goku3012 <jennifer.pata.mina@utelvt.edu.ec>
2022-05-23 17:31:26 -0500

    Actualización ACTIVIDAD-A2

--- ACTIVIDAD-A2/PJ_libreria.h
+++ ACTIVIDAD-A2/PJ_libreria.h
@@ -5,26 +5,27 @@
 //
 //
 
-int PJ_SumaN();
+int PJ_SumaN()
 {
-cout<<"//======================"<<endl;
-cout<<"//============> Nombre del Programa: Suma de Varios Números"<<endl;
-cout<<"//============> Archivo: SumaN.cpp"<<endl;
-cout<<"//============> Autor: Pata Mina Jennifer Maibe"<<endl;
-cout<<"//============> Fecha de Creación: 23/04/2022"<<endl;
-cout<<"//============> Fecha de Actualización: 18/05/2022"<<endl;
-cout<<"//================"<<endl<<endl;
+
+	cout<<"//================================================="<<endl;
+	cout<<"//============> Nombre del Programa: Suma de Varios Números"<<endl;
+	cout<<"//============> Archivo: SumaN.cpp"<<endl;
+	cout<<"//============> Autor: Pata Mina Jennifer Maibe"<<endl;
+	cout<<"//============> Fecha de Creación: 23/04/2022"<<endl;
+	cout<<"//============> Fecha de Actualización: 18/05/2022"<<endl;
+	cout<<"//================================================="<<endl<<endl;
 
         int MJ_c=0,MJ_n;
 
         float MJ_a=0,MJ_x;
 
-        cout<<"Ingrese la Cantidad de Números a Sumar: ";
+        cout<<"Ingrese la Cantidad de Números a Sumar: "<<endl;
         cin>>MJ_n;
 
         do{
 
-        cout<<"Ingrese un Número: ";
+        cout<<"Ingrese un Número: "<<endl;
         cin>>MJ_x;
 
         MJ_c=MJ_c+1;
@@ -40,74 +41,74 @@ cout<<"//================"<<endl<<endl;
 
 
 
-int PJ_Compara();
-
+int PJ_Compara()
 {
 
-cout<<"//======================"<<endl;cout<<"//===> Nombre del Programa:Comparación de 2 números"<<endl;
-cout<<"//===> Archivo: Compara.cpp"<<endl;
-cout<<"//===> Autor: Pata Mina Jennifer Maibe"<<endl;
-cout<<"//===> Fecha de Creación: 23/04/2022"<<endl;
-cout<<"//===> Fecha de Actualización: 17/05/2022"<<endl;
-cout<<"//================"<<endl<<endl;
+	cout<<"//=========================================================="<<endl;
+	cout<<"//===> Nombre del Programa:Comparación de 2 números"<<endl;
+	cout<<"//===> Archivo: Compara.cpp"<<endl;
+	cout<<"//===> Autor: Pata Mina Jennifer Maibe"<<endl;
+	cout<<"//===> Fecha de Creación: 23/04/2022"<<endl;
+	cout<<"//===> Fecha de Actualización: 17/05/2022"<<endl;
+	cout<<"//==========================================================="<<endl<<endl;
 
         float  MJ_x,MJ_y;
 
- cout<<"Ingrese el valor de X: ";
+ 	cout<<"Ingrese el valor de X: "<<endl;
         cin>>MJ_x;   
 
-cout<<"Ingrese el valor de Y: ";
+	cout<<"Ingrese el valor de Y: "<<endl;
         cin>>MJ_y;
 
         if(MJ_x==MJ_y){
 
-cout<<"El valor de X: "<<MJ_x<<"es igual a Y: "<<MJ_y<<endl;
+	cout<<"El valor de X: "<<MJ_x<<" es igual a Y: "<<MJ_y<<endl;
         }else{
 
         if(MJ_x<MJ_y){
 
- cout<<"El valor de X: "<<MJ_x<<" es menor a Y: "<<MJ_y<<endl;
+	 cout<<"El valor de X: "<<MJ_x<<" es menor a Y: "<<MJ_y<<endl;
         }else{
 
- cout<<"El valor de Y: "<<MJ_y<<" es menor a X: "<<MJ_x<<endl;
+	 cout<<"El valor de Y: "<<MJ_y<<" es menor a X: "<<MJ_x<<endl;
         }
         }
         return 0;
 }
 
 
-int PJ_PuntoVenta();
-
+int PJ_PuntoVenta()
 {
 
-cout<<"//======================"<<endl;cout<<"//==========> Nombre del Programa: Punto de Venta de varios Productos"<<endl;
-cout<<"//==========> Archivo: PuntoVenta.cpp"<<endl;
-cout<<"//==========> Autor: Pata Mina Jennifer Maibe"<<endl;
-cout<<"//==========> Fecha de Creación: 23/04/2022"<<endl;
-cout<<"//==========> Fecha de Actualización: 19/05/2022"<<endl;
-cout<<"//========================="<<endl;
+	cout<<"//======================================================================"<<endl;
+	cout<<"//==========> Nombre del Programa: Punto de Venta de varios Productos"<<endl;
+	cout<<"//==========> Archivo: PuntoVenta.cpp"<<endl;
+	cout<<"//==========> Autor: Pata Mina Jennifer Maibe"<<endl;
+	cout<<"//==========> Fecha de Creación: 23/04/2022"<<endl;
+	cout<<"//==========> Fecha de Actualización: 19/05/2022"<<endl;
+	cout<<"//======================================================================"<<endl<<endl;
 
         int MJ_c=0,MJ_n;
 
         float MJ_a=0,MJ_x,MJ_Piva,MJ_Pdsc,MJ_iva=0.12,MJ_dsc=0.10,MJ_vf;
 
-cout<<"Ingrese la Cantidad de Productos a Comprar: ";
+	cout<<"Ingrese la Cantidad de Productos a Comprar: "<<endl;
         cin>>MJ_n;
         do{
-cout<<"Ingrese el valor del Producto:$ ";
+	cout<<"Ingrese el valor del Producto:$ "<<endl;
                 cin>>MJ_x;
 
                 MJ_c=MJ_c+1;
                 MJ_a=MJ_a+MJ_x;
                 MJ_Piva=MJ_a*MJ_iva;
 
-cout<<"El Valor del Iva de su compra es: $ "<<MJ_Piva<<endl;
+	cout<<"El Valor del Iva de su compra es de: $ "<<MJ_Piva<<endl;
         MJ_Pdsc=MJ_a*MJ_dsc;
 
-cout<<"El valor del Descuento de su Compra es: $ "<<MJ_Pdsc<<endl;  
+	cout<<"El valor del Descuento de su Compra es de: $ "<<MJ_Pdsc<<endl;  
 
         }while(MJ_c<MJ_n);{                                                                                           
-        MJ_vf=MJ_a+MJ_Piva-MJ_Pdsc;                                                                                  cout<<"El Valor Total de su Compra a pagar es: $ "<<MJ_vf<<endl;
+        MJ_vf=MJ_a+MJ_Piva-MJ_Pdsc;                                                                                        cout<<"El Valor Total de su Compra a Pagar es de: $ "<<MJ_vf<<endl;
         }
         return 0;
 }
@@ -115,30 +116,29 @@ cout<<"El valor del Descuento de su Compra es: $ "<<MJ_Pdsc<<endl;
 
 
 
-int PJ_CuentaMoneda();
-
+int PJ_CuentaMoneda()
 {
 
-cout<<"//======================"<<endl;
-cout<<"//=======> Nombre del Programa: Cuenta Monedas DonNaza"<<endl;
-cout<<"//=======> Archivo: CuentaMoneda.cpp"<<endl;               
-cout<<"//=======> Autor: Pata Mina Jennifer Maibe"<<endl;
-cout<<"//=======> Fecha de Creación:23/04/2022"<<endl;
-cout<<"//=======> Fecha de Actualización:19/05/2022"<<endl;
-cout<<"//======================"<<endl;
+	cout<<"//========================================================="<<endl;
+	cout<<"//=======> Nombre del Programa: Cuenta Monedas Don Naza"<<endl;
+	cout<<"//=======> Archivo: CuentaMoneda.cpp"<<endl;               
+	cout<<"//=======> Autor: Pata Mina Jennifer Maibe"<<endl;
+	cout<<"//=======> Fecha de Creación:23/04/2022"<<endl;
+	cout<<"//=======> Fecha de Actualización:19/05/2022"<<endl;
+	cout<<"//========================================================="<<endl<<endl;
 
         int MJ_n,MJ_c=0,MJ_c1=0,MJ_c2=0,MJ_c3=0;
 
         float  MJ_x,MJ_a=0,MJ_a1=0,MJ_a2=0,MJ_a3=0;
 
-cout<<"Bienvenidos al Cuenta Monedas Don Naza "<<endl<<endl;
+	cout<<"Bienvenidos al Cuenta Monedas Don Naza "<<endl<<endl;
 
-cout<<"Ingrese la Cantidad de monedas a contar: "<<endl<<endl;
+	cout<<"Ingrese la Cantidad de monedas a contar: "<<endl;
         cin>>MJ_n;
 
         do{
 
-cout<<"Ingrese su valor en monedas (0.10,0.25,0.50):$ "<<endl;
+	cout<<"Ingrese su valor en monedas (0.10,0.25,0.50):$ "<<endl<<endl;
                 cin>>MJ_x;
 
                 MJ_c=MJ_c+1;
@@ -160,38 +160,39 @@ cout<<"Ingrese su valor en monedas (0.10,0.25,0.50):$ "<<endl;
                         
 	}while(MJ_c<MJ_n);
 
-cout<<"El Resultado Fue: "<<endl<<endl;
-cout<<"Total de Monedas Contadas: "<<MJ_c<<endl<<endl;
-cout<<"Valor Total en Efectivo:$ "<<MJ_a<<endl<<endl;
-cout<<"Cantidad de monedas de 0.10ctvs: "<<MJ_c1<<endl;
-cout<<"Valor en Efectivo de monedas de 0.10ctvs:$ "<<MJ_a1<<endl<<endl;
-cout<<"Cantidad de monedas de 0.25ctvs: "<<MJ_c2<<endl;
-cout<<"Valor en Efectivo de monedas de 0.25ctvs:$ "<<MJ_a2<<endl<<endl;
-cout<<"Cantidad de monedas de 0.50ctvs: "<<MJ_c3<<endl<<endl;
-cout<<"Valor en Efectivo de monedas de 0.50ctvs:$ "<<MJ_a3<<endl;
+	cout<<"El Resultado Fue: "<<endl;
+	cout<<"Total de Monedas Contadas: "<<MJ_c<<endl;
+	cout<<"Valor Total en Efectivo:$ "<<MJ_a<<endl<<endl;
+	cout<<"Cantidad de monedas de 0.10ctvs: "<<MJ_c1<<endl;
+	cout<<"Valor en Efectivo de monedas de 0.10ctvs:$ "<<MJ_a1<<endl;
+	cout<<"Cantidad de monedas de 0.25ctvs: "<<MJ_c2<<endl;
+	cout<<"Valor en Efectivo de monedas de 0.25ctvs:$ "<<MJ_a2<<endl;
+	cout<<"Cantidad de monedas de 0.50ctvs: "<<MJ_c3<<endl;
+	cout<<"Valor en Efectivo de monedas de 0.50ctvs:$ "<<MJ_a3<<endl;
 
         return 0;
 }
 
 
 
-int PJ_Edad();
-
+int PJ_Edad()
 {
 
-cout<<"//======================"<<endl;                                       cout<<"//========> Nombre del Programa: Calcular la edad de una Persona"<<endl;
-cout<<"//========> Archivo:laedad.cpp"<<endl;
-cout<<"//========> Autor: Pata Mina Jennifer Maibe"<<endl;
-cout<<"//========> Fecha de Creación:23/04/2022"<<endl;
-cout<<"//========> Fecha de Actualización:19/05/2022"<<endl;
-cout<<"//================"<<endl<<endl;
+	cout<<"//================================================================"<<endl;                                  cout<<"//========> Nombre del Programa: Calcular la edad de una Persona"<<endl;
+	cout<<"//========> Archivo:laedad.cpp"<<endl;
+	cout<<"//========> Autor: Pata Mina Jennifer Maibe"<<endl;
+	cout<<"//========> Fecha de Creación:23/04/2022"<<endl;
+	cout<<"//========> Fecha de Actualización:19/05/2022"<<endl;
+	cout<<"//================================================================="<<endl<<endl;
+
+
 
         int MJ_da,MJ_ma,MJ_aa,MJ_dn,MJ_mn,MJ_an,MJ_d,MJ_m,MJ_a;
 
-cout<<"Ingrese la Fecha Actual: Dia Mes  Año "<<endl;
+	cout<<"Ingrese la Fecha Actual: Día Mes  Año "<<endl;
        cin>>MJ_da>>MJ_ma>>MJ_aa;
 
-cout<<"Ingrese la Fecha de su  Nacimiento: Dia Mes  Año "<<endl;
+	cout<<"Ingrese la Fecha de su  Nacimiento: Día Mes  Año "<<endl;
         cin>>MJ_dn>>MJ_mn>>MJ_an;
 
         if(MJ_da<MJ_dn){
@@ -210,17 +211,18 @@ cout<<"Ingrese la Fecha de su  Nacimiento: Dia Mes  Año "<<endl;
 		MJ_m=MJ_ma-MJ_mn;                   
 	}                           
 	        MJ_a=MJ_aa-MJ_an;                          
-cout<<"Ustd Tiene "<<MJ_a<<" años "<<MJ_m<<" meses "<<" y "<<MJ_d<<" dias "<<endl;            
+	
+	cout<<"Ustd Tiene "<<MJ_a<<" años "<<MJ_m<<" meses "<<" y "<<MJ_d<<" días "<<endl;            
 
-return 0;                           
+	return 0;                           
 }
 
 
 
 
 int PJ_menu()
-
 {
+
 	int op,flag;
 
 	do{
@@ -228,15 +230,14 @@ int PJ_menu()
 
 		cout<<"+++++ Bienvenidos +++++"<<endl;
 		cout<<"***** Programas de PataJennifer *****"<<endl<<endl;
-                cout<<" 1.- Suma de varios Números \n.";
-                cout<<" 2.-Comparacion de 2 Números";
-                cout<<" 3.-Cuenta Monedas Don Naza";
-               cout<<" 4.-Calculo de Edades";
-                cout<<" 5.-Punto de Venta Productos varios";
-
-        cout<<" 0.- Salir del menú \n.";
-        cout<<" Ingrese su opción: "<<endl;
-        cin>>op;
+                cout<<" 1.- Suma de varios Números"<<endl;
+                cout<<" 2.-Comparación de 2 Números"<<endl;
+                cout<<" 3.-Cuenta Monedas Don Naza"<<endl;
+                cout<<" 4.-Calculadora de Edades"<<endl;
+                cout<<" 5.-Punto de Venta varios Productos"<<endl;
+		cout<<" 0.- Salir del menú"<<endl<<endl;
+        	cout<<" Ingrese su opción: "<<endl;
+       		 cin>>op;
         switch(op){
 
                 case 1:
@@ -246,35 +247,35 @@ int PJ_menu()
 
                 case 2:
 			system("clear");
-			PJ_Compara()
+			PJ_Compara();
 
                         break;
 
                 case 3:
 			system("clear");
-			PJ_CuentaMoneda()
+			PJ_CuentaMoneda();
 
                         break;
 
                 case 4:
 			system("clear");
-			PJ_Edad()
+			PJ_Edad();
 
                         break;
 
                 case 5:
 			system("clear");
-			PJ_PuntoVenta()
+			PJ_PuntoVenta();
 
                         break;
         }
 
         if(op!=0 && op>5){
-                cout<<"\nopción NO permitida! \n";
+                cout<<"Opción NO permitida! "<<endl;
         }
 
         if(op==0){
-                cout<<"Deseas Salir  de GeneraciónZ (presiona enter): "<<endl;
+                cout<<"Deseas Salir del Menú de Programas  (presiona enter): "<<endl;
         }
 	
 
--- ACTIVIDAD-A2/ProgramaPrincipal.cpp
+++ ACTIVIDAD-A2/ProgramaPrincipal.cpp
@@ -1,14 +1,11 @@
 //=====> Autores: Pata Jennifer,
-//=====> Fecha de Creación: 22 05/2022
-//=====> Fecha de Actualización: 
+//=====> Fecha de Creación: 22/05/2022
+//=====> Fecha de Actualización:23/05/2022 
 
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
-
 #include"PJ_libreria.h"
-
-
 
 int main()
 
@@ -20,18 +17,18 @@ int main()
 
 	cout<<"+*+*+*+* Generación Z  +*+*+*+*"<<endl<<endl;
 
-	cout<<" 1.- Programas de Pata Jennifer \n.";
-	cout<<" 2.-";
-	cout<<" 3.-";
-	cout<<" 4.-";
-	cout<<" 5.-";
-
-	cout<<" 0.- Salir del menú \n.";
+	cout<<" 1.- Programas de Pata Jennifer"<<endl;
+	cout<<" 2.-"<<endl;
+	cout<<" 3.-"<<endl;
+	cout<<" 4.-"<<endl;
+	cout<<" 5.-"<<endl;
+	cout<<" 0.- Salir del menú"<<endl<<endl;
 	cout<<" Ingrese su opción: "<<endl;
 	cin>>op;
 	switch(op){
 
 		case 1:
+			system("clear");
 			PJ_menu();
 			break;
 
@@ -53,16 +50,16 @@ int main()
 	}
 
 	if(op!=0 && op>5){
-		cout<<"\nopción NO permitida! \n";
+		cout<<"Opción NO permitida! "<<endl;
 	}
 
 	if(op==0){
-		cout<<"Deseas Salir  de GeneraciónZ (presiona enter): "<<endl;
+		cout<<"Deseas Salir  de Generación Z (presiona enter): "<<endl;
 	}
 
-	flag=cin.ignore().get();
+	flag=cin.ignore(0).get();
 }while(op!=0);
 	return(0);
 }
 
- 
+
