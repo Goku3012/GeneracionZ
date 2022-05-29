e53fbad04bd96a3cfb1b79a4fb8fce9cc78c4272
DanielaMontesdeoca <mishellecmontesdeoca@gmail.com>
2022-05-29 17:40:19 -0500

    Actualizacion-A2

--- ACTIVIDAD-A2/MD_libreria.h
+++ ACTIVIDAD-A2/MD_libreria.h
@@ -86,11 +86,15 @@ cout<<"//==============================================="<<endl<<endl;
 
 int MD_SumaN()
 {
-	int MD_c=0,MD_a;                                           float MD_s=0,MD_b;                                         cout<<"Ingrese la cantidad de numeros a sumar: ";          cin>>MD_a;                                                 do{                                                        cout<<"Ingrese un nunero: ";                               cin>>MD_b;                                                 MD_c=MD_c+1;                                               MD_s=MD_s+MD_b;                                            }while(MD_c<MD_a);                                                                                            cout<<"//==============================================="<<endl;      
-cout<<"//===>Nombre del programa: Suma de 2 nuneros"<<endl;cout<<"//===>Archivo:SumaN.cpp"<<endl;                     cout<<"//===>Autor:Montesdeoca Castillo Daniela Mishelle"<<endl;      
-cout<<"//===>Fecha de elaboracion:02/05/2022"<<endl;       cout<<"//===>Fecha de actualizacion:17/05/2022"<<endl;
+	int MD_c=0,MD_a;                                      float MD_s=0,MD_b;                                    cout<<"Ingrese la cantidad de numeros a sumar: ";     
+	cin>>MD_a;                                            do{                                                   cout<<"Ingrese un nunero: ";                          cin>>MD_b;                                            MD_c=MD_c+1;                                          MD_s=MD_s+MD_b;                                       }while(MD_c<MD_a);                                                                                  cout<<"//==============================================="<<endl;      
+cout<<"//===>Nombre del programa: Suma de 2 nuneros"<<endl;
+cout<<"//===>Archivo:SumaN.cpp"<<endl;           
+cout<<"//===>Autor:Montesdeoca Castillo Daniela Mishelle"<<endl;      
+cout<<"//===>Fecha de elaboracion:02/05/2022"<<endl;  cout<<"//===>Fecha de actualizacion:17/05/2022"<<endl;
 cout<<"//==============================================="<<endl<<endl;              
-          cout<<"La suma total es: "<<MD_s<<endl;                               return 0;
+          cout<<"La suma total es: "<<MD_s<<endl;               
+	  return 0;
 }
 
 int MD_Puntoventa()
